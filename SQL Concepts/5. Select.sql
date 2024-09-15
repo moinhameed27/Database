02ea79c8ac12a0c1b1f91ce71263cdf3de4bb330
Moin Hameed <moinhameed27@gmail.com>
2024-09-15 12:00:02 +0500

    SELECT / DISTINCT

---
+++ SQL Concepts/5. Select.sql
@@ -0,0 +1,13 @@
+select * from student;
+
+/*
+SELECT : selects columns from the table.
+* selects all columns
+1. If you want to select specific columns, write name of columns.
+*/
+
+select rollno, name, grade from student;
+
+/* DISTINCT : Selects only unique data from any column. */
+select distinct grade from student;
+
