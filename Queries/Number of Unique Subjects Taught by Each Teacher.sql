5cbaacb7a23726abda265cdb530e3849afcfa22c
Moin Hameed <moinhameed27@gmail.com>
2024-09-15 20:31:42 +0500

    SQL (1)

---
+++ Queries/Number of Unique Subjects Taught by Each Teacher.sql
@@ -0,0 +1,6 @@
+/* Leetcode : https://leetcode.com/problems/number-of-unique-subjects-taught-by-each-teacher/description/ */
+
+/* Way - I */
+Select  teacher_id, 
+count(distinct subject_id ) as cnt from Teacher
+group by teacher_id
