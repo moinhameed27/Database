41bbada59b597d5b465fbe5f8624a786488c7efe
moin <moinhameed27@gmail.com>
2024-10-10 09:21:21 +0500

    SQL (3)

--- Queries/Basic JOINS/Customer Who Visited but Did Not Make Any Transactions.sql
+++
@@ -1,8 +0,0 @@
-/* Leetcode : https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/description/ */ 
-
-select V.customer_id, count(V.visit_id) as count_no_trans
-from Visits as V
-left join Transactions as T
-on V.visit_id = T.visit_id
-where T.transaction_id is null
-group by customer_id
--- Queries/Basic JOINS/Employee Bonus.sql
+++
@@ -1,7 +0,0 @@
-/* Leetcode : https://leetcode.com/problems/employee-bonus/description/ */ 
-
-select E.name, B.bonus
-from Employee as E
-left join Bonus as B
-on E.empId = B.empId 
-where B.bonus < 1000 or B.bonus is null
