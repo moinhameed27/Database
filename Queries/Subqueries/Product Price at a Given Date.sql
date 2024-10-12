b956b0ed3da9694c8e4006bd2ff31c21693a14b8
moin <moinhameed27@gmail.com>
2024-10-12 10:41:10 +0500

    SQL

--- Queries/Subqueries/Product Price at a Given Date.sql
+++ Queries/Subqueries/Product Price at a Given Date.sql
@@ -1,8 +1,17 @@
 /* Leetcode : https://leetcode.com/problems/product-price-at-a-given-date/description/ */
 
 /* Price is 10 for products having change_date after "2019-08-16" */
+
+/* Way - I */
 select distinct product_id, 10 as price from Products 
 where product_id not in (select distinct product_id from Products where change_date <= "2019-08-16")
+union
+select product_id, new_price as price  from Products
+where (product_id, change_date) in (select product_id, max(change_date) from Products where change_date <= "2019-08-16" group by product_id)
+
+/* Way - II */
+select distinct product_id, 10 as price from Products 
+group by product_id having min(change_date) > "2019-08-16"
 union
 select product_id, new_price as price  from Products
 where (product_id, change_date) in (select product_id, max(change_date) from Products where change_date <= "2019-08-16" group by product_id)
