e371b866d9bfbffd86b17448b63ca54558d6418a
moin <moinhameed27@gmail.com>
2024-10-14 12:27:05 +0500

    SQL

---
+++ Queries/Subqueries/Employees Whose Manager Left the Company.sql
@@ -0,0 +1,6 @@
+/* Leetcode : https://leetcode.com/problems/employees-whose-manager-left-the-company/description/ */
+
+select employee_id from Employees
+where salary < 30000 and 
+manager_id not in (select employee_id from Employees)
+order by employee_id
